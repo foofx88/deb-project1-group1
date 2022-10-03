a66ff21dbec16f0379bf1c0942623849a5924efd
Luke Huntley <68954662+LuckyLukeAtGitHub@users.noreply.github.com>
2022-10-03 17:29:41 +0800

    Delete config.bat
    As per PR review, I've deleted the config.bat file.

--- OzWeathers/src/weatherapi/config.bat
+++
@@ -1,8 +0,0 @@
-set source_db_user=PostgresAdmin
-set source_db_password=v3h9vJ8fG^^^&AqOMNi
-set source_db_server_name=weather.cgtcb5sorecc.ap-southeast-2.rds.amazonaws.com
-set source_db_database_name=weather
-set target_db_user=PostgresAdmin
-set target_db_password=v3h9vJ8fG^^^&AqOMNi
-set target_db_server_name=weather.cgtcb5sorecc.ap-southeast-2.rds.amazonaws.com
-set target_db_database_name=weather
